b2a82bf254015f98d4dca4a0b7ff87fd7696c869
Isabel Lopes <Isabelengelienlopes@outlook.com>
2026-04-30 12:31:35 +0200

    legger inn const for api
    legger inn const for api

---
+++ detaljegrafer/scriptDetalje.js
@@ -0,0 +1,5 @@
+const DATA_API =
+    "https://data.ssb.no/api/pxwebapi/v2/tables/07459/data";
+
+const KOMMUNE_API =
+    "https://data.ssb.no/api/klass/v1/classifications/131/codes?from=2020-01-01";
