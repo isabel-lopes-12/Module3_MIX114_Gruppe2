95e85c2156893b3c98688ed056c61c376f4f0743
Isabel Lopes <Isabelengelienlopes@outlook.com>
2026-05-04 12:34:24 +0200

    henter kommuner
    henter kommuner

--- sammenligningsgrafer/scriptSammenligning2.js
+++ sammenligningsgrafer/scriptSammenligning2.js
@@ -4,3 +4,20 @@ const DATA_API =
 let valgte = [];
 let alleKommuner = [];
 let filtrertListe = [];
+
+async function hentKommuner() {
+    const res = await fetch(
+    "https://data.ssb.no/api/klass/v1/classifications/131/codes?from=2020-01-01"
+    );
+    const json = await res.json();
+
+    alleKommuner = json.codes
+        .filter(k => k.code.length === 4)
+        .map(k => ({
+        kode: k.code,
+        navn: k.name
+    }))
+    .sort((a, b) => a.navn.localeCompare(b.navn));
+
+    filtrertListe = alleKommuner;
+}
